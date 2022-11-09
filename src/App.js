05b24cadf293bcb62ec6ed5b83d365668eaff2aa
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2022-11-08 19:27:34 -0500

    add middle navigation section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Hero from './components/Hero';
+import MiddleNav from './components/MiddleNav';
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="bg-[url('https://images.pexels.com/photos/1448645/pexels-photo-1448645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] bg-no-repeat bg-cover bg-center"> 
-      <Navbar />
-      <Hero />
+    <div>
+      <div className="bg-[url('https://images.pexels.com/photos/1448645/pexels-photo-1448645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] bg-no-repeat bg-cover bg-center"> 
+        <Navbar />
+        <Hero />
+      </div>
+      
+      <MiddleNav />
     </div>
   );
 }
