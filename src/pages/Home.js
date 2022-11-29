4d78a080bf5be711fd9e1eed2bd3cb483bb32976
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2022-11-29 15:16:34 -0500

    Titles added to pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,10 @@ import ImageGrid from '../components/ImageGrid';
 
 const Home = ({ searchTerm }) => {
   return (
-    <ImageGrid searchTerm={searchTerm}/>
+    <div>
+      <h1 className='text-2xl text-slate-500 font-semibold py-4 px-6 md:px-10'>Free Stock Photos</h1>
+      <ImageGrid searchTerm={searchTerm}/>
+    </div>
   )
 }
 
--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -3,7 +3,10 @@ import VideoGrid from '../components/VideoGrid';
 
 const Videos = ({ searchTerm }) => {
   return (
-    <VideoGrid searchTerm={searchTerm} />
+    <div>
+      <h1 className='text-2xl text-slate-500 font-semibold py-4 px-6 md:px-10'>Free Stock Videos</h1>
+      <VideoGrid searchTerm={searchTerm} />
+    </div>
   )
 }
 
